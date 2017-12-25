52c0a8ba16c0f9869fe2a5465ccb2a592408f912
HuYuHsiang <hsiang851108@gmail.com>
2017-12-25 23:05:45 +0800

    Fixed sql error.

--- SQL/initialize.sql
+++ SQL/initialize.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- 主機:                           127.0.0.1
--- 服務器版本:                        5.7.8-rc-log - MySQL Community Server (GPL)
--- 服務器操作系統:                      Win64
--- HeidiSQL 版本:                  9.4.0.5125
+-- 伺服器版本:                        5.7.20-log - MySQL Community Server (GPL)
+-- 伺服器操作系統:                      Win64
+-- HeidiSQL 版本:                  9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -12,11 +12,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- 導出 pass 的資料庫結構
+-- 傾印 pass 的資料庫結構
 CREATE DATABASE IF NOT EXISTS `pass` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `pass`;
 
--- 導出  表 pass.homeworks 結構
+-- 傾印  表格 pass.homeworks 結構
+DROP TABLE IF EXISTS `homeworks`;
 CREATE TABLE IF NOT EXISTS `homeworks` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '0',
@@ -31,11 +32,12 @@ CREATE TABLE IF NOT EXISTS `homeworks` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在導出表  pass.homeworks 的資料：~0 rows (大約)
+-- 正在傾印表格  pass.homeworks 的資料：~0 rows (大約)
 /*!40000 ALTER TABLE `homeworks` DISABLE KEYS */;
 /*!40000 ALTER TABLE `homeworks` ENABLE KEYS */;
 
--- 導出  表 pass.submit_homeworks 結構
+-- 傾印  表格 pass.submit_homeworks 結構
+DROP TABLE IF EXISTS `submit_homeworks`;
 CREATE TABLE IF NOT EXISTS `submit_homeworks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `teacher_assignment_id` int(11) NOT NULL,
@@ -47,11 +49,12 @@ CREATE TABLE IF NOT EXISTS `submit_homeworks` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在導出表  pass.submit_homeworks 的資料：~0 rows (大約)
+-- 正在傾印表格  pass.submit_homeworks 的資料：~0 rows (大約)
 /*!40000 ALTER TABLE `submit_homeworks` DISABLE KEYS */;
 /*!40000 ALTER TABLE `submit_homeworks` ENABLE KEYS */;
 
--- 導出  表 pass.user 結構
+-- 傾印  表格 pass.user 結構
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(20) NOT NULL,
@@ -62,14 +65,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
--- 正在導出表  pass.user 的資料：~7 rows (大約)
+-- 正在傾印表格  pass.user 的資料：~0 rows (大約)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `user_id`, `name`, `password`, `role`, `email`) VALUES
-	(1passhomeworks, '1', 'admin', '1', 'administrator', 'admin@gmail.com'),
+REPLACE INTO `user` (`id`, `user_id`, `name`, `password`, `role`, `email`) VALUES
+	(1, '1', 'admin', '1', 'administrator', 'admin@gmail.com'),
 	(2, '123', 'Happy123', '321', 'instructor', 'happy@mail.com'),
-	(3, '0', '崔智賢', '0', 'student', 'studen@gmail.com'),
+	(3, '0', '崔智賢', '0', 'student', 'student@gmail.com'),
 	(4, '2', '旻孝援', '2', 'student', 'student2@gmail.com'),
-	(5, '3', '崔茂宣', '3', 'student', 'student3@gmail.com'),
+	(5, '3', '崔茂宣', '0', 'student', 'student3@gmail.com'),
 	(6, '4', '李怋稿', '4', 'student', 'student4@gmail.com'),
 	(7, '5', '織田信長', '5', 'student', 'student5@gmail.com');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
